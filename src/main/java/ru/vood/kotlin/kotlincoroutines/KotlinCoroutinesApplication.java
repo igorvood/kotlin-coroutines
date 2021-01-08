336dcf0cf882870b36cd5c7aab6a6544cda75c8f
balitsky_igor <balitsky_igor@list.ru>
2021-01-08 13:32:16 +0300

    init

---
+++ src/main/java/ru/vood/kotlin/kotlincoroutines/KotlinCoroutinesApplication.java
@@ -0,0 +1,13 @@
+package ru.vood.kotlin.kotlincoroutines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinCoroutinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotlinCoroutinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vood/kotlin/kotlincoroutines/KotlinCoroutinesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vood.kotlin.kotlincoroutines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinCoroutinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
